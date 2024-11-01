2a97ee3ef0c0bad7d8390f71aa45593b37c785d5
sree :) <kandula.s@northeastern.edu>
2024-11-01 16:45:38 -0400

    Initialized react files

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-mono underline"><i> The Lynnwood <span className="text-yellow-500">Gold</span><br />Forsythia Shrub </i></h1>
+      {/* <br />
+      <br />
+      <div className="border-2, border-black rounded-2xl h-80 w-6/12 bg-green-900 flex drop-shadow-lg">
+        <div className="h-80 w-4/12 text-slate-50"> hello </div>
+        <div className=" h-80 w-8/12 bg-slate-50 rounded-r-2xl"></div>
+        
+      </div> */}
+
+
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
